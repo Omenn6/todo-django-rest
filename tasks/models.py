29047b6641f04f0feaba35dbad861947b451e9cf
Pavel <bruchkovskiiy@mail.ru>
2026-04-28 23:59:57 +0300

    feat: add custom User model and reset migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+
+
+class User(AbstractUser):
+    pass
 
 
 class Category(models.Model):
