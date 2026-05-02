6b9ca618cd12b4921f5415008028992933b60cd7
Pavel <bruchkovskiiy@mail.ru>
2026-05-02 20:15:46 +0300

    feat: add statistics to TaskViewSet

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,10 @@
-from rest_framework import viewsets, permissions, filters
+from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import viewsets, permissions, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from .models import Category, Label, Task, Comment
 from .serializers import CategorySerializer, LabelSerializer, TaskSerializer, CommentSerializer
 
@@ -28,6 +33,23 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=False, methods=['get'])
+    def stats(self, request):
+        queryset = self.get_queryset()
+
+        total_tasks = queryset.count()
+        completed_tasks = queryset.filter(is_completed=True).count()
+
+        priority_stats = queryset.values('priority').annotate(count=Count('id'))
+
+        return Response({
+            'total_tasks': total_tasks,
+            'completed_tasks': completed_tasks,
+            'priority_distribution': {
+                str(item['priority']): item['count'] for item in priority_stats
+            }
+        })
 
 
 class CommentViewSet(viewsets.ModelViewSet):
